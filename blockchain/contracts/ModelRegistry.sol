23db11899008d308ddb9b7961e2429fb948f0ea1
amandx36 <xamandeep360@gmail.com>
2026-03-06 21:25:27 +0530

    Improved  contract with validation

--- blockchain/contracts/ModelRegistry.sol
+++ blockchain/contracts/ModelRegistry.sol
@@ -5,9 +5,11 @@ contract ModelRegistry {
     // 1 Struct for creating model info
     struct Model {
         string name;
-        uint256 id;
+        uint256 modelId;
         string ipfsHash;
+        uint256 _price  ;
         address owner;
+        bool active ;
     }
 
     // 2    Create a counter to generate unique model IDs
@@ -18,15 +20,36 @@ contract ModelRegistry {
     // 4   Function to register a model
     function registerModel(
         string memory _name,
-        string memory _ipfsHash
+        string memory _ipfsHash,
+        uint256 _price  
     ) public {
         modelCount++;
 
-        models[modelCount] = Model(_name, modelCount, _ipfsHash, msg.sender);
+        models[modelCount] = Model(_name, modelCount, _ipfsHash, _price  ,  msg.sender, true );
     }
 
-    //  5 Function to retrieve a model
-    function getModel(uint256 _modelId) public view returns (Model memory) {
+    // Get model details
+    function getModel(uint256 _modelId)
+        public
+        view
+        returns (Model memory)
+    {
+        require(_modelId > 0 && _modelId <= modelCount, "Model not found");
+
         return models[_modelId];
     }
+    // Transfer ownership of a model
+   function transferModel(uint256 _modelId, address _newOwner) public {
+
+    require(_modelId > 0 && _modelId <= modelCount, "Model not found");
+
+    Model storage model = models[_modelId];
+
+    require(msg.sender == model.owner, "Only owner can transfer");
+    require(model.active == true, "Model inactive");
+
+    model.owner = _newOwner;
+}   
+
+
 }
